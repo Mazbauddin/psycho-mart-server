952292fc543b1868daf2ced6adb47cbfee7e09f2
Mazba Uddin Shawon <mazbauddin2010@gmail.com>
2026-06-19 19:50:38 +0600

    add app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // start here
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+
+app.use(cors());
+app.options("*", cors());
